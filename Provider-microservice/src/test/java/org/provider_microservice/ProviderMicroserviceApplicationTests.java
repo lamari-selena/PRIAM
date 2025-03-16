c18ec4198b71080730635cf775d86bba5e454511
Selena Lamari <lamari.selena.info@gmail.com>
2025-03-17 00:26:50 +0100

    update ms

---
+++ Provider-microservice/src/test/java/org/provider_microservice/ProviderMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.provider_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
