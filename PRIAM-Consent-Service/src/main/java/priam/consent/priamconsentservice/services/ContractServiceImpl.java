1ed30c380e324a93f6195c22347b5a7df1731e79
Selena Lamari <lamari.selena.info@gmail.com>
2025-05-14 16:32:03 +0200

    update

--- PRIAM-Consent-Service/src/main/java/priam/consent/priamconsentservice/services/ContractServiceImpl.java
+++ PRIAM-Consent-Service/src/main/java/priam/consent/priamconsentservice/services/ContractServiceImpl.java
@@ -74,10 +74,12 @@ public class ContractServiceImpl implements ContractService{
     public Map<String, Boolean> getConsentByDataSubject(List<String> idRefList, String processingId){
         // A supprimer
         if (processingId.equals("RecommenderSelector.recommendProducts")) processingId = "1";
+        if (processingId.equals("CartServlet.handleGETRequest")) processingId = "3";
         //
         Map<String, Boolean> listDsAndConsent = new HashMap<>();
         for (String dataSubjectIdRef: idRefList) {
             List<ConsentResponseDTO> consents = getListConsentByDataSubject(dataSubjectIdRef, processingId);
+            System.out.println("------------------------" + dataSubjectIdRef+ "----------=" +  processingId);
             for (ConsentResponseDTO c: consents
                  ) {
                 if (c.getEndDate()==null) {
